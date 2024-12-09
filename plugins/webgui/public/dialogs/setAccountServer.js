389a722121a931fd7f1eb94bee0265a893568d72
MicroGalaxy <327288751@qq.com>
2024-12-10 02:05:56 +0800

    [Add] Region select

--- plugins/webgui/public/dialogs/setAccountServer.js
+++ plugins/webgui/public/dialogs/setAccountServer.js
@@ -36,12 +36,30 @@ app.factory('setAccountServerDialog' , [ '$mdDialog', $mdDialog => {
         }
         return { 'min-width': '405px' };
       };
+      $scope.$watch('publicInfo.selectedRegion', function (newRegion, oldRegion) {
+        if (newRegion) {
+          // 清空所有选中的状态
+          Object.keys($scope.publicInfo.account.accountServerObj).forEach(function (key) {
+            $scope.publicInfo.account.accountServerObj[key] = false;
+          });
+
+          // 根据选中的地区更新对应服务器的选中状态
+          $scope.publicInfo.servers.forEach(function (server) {
+            let region = JSON.parse(server.comment).region; // 解析 server.comment 获取 region
+            if (region === newRegion) {
+              $scope.publicInfo.account.accountServerObj[server.id] = true;
+            }
+          });
+        }
+      }, true);
     }],
     clickOutsideToClose: true,
   };
   const show = (account, servers) => {
     publicInfo.account = account;
     publicInfo.servers = servers;
+    publicInfo.regions = [...new Set(servers.map(element => JSON.parse(element.comment).region))]; // 去重地区列表
+    publicInfo.selectedRegion = '';
     if(isDialogShow()) {
       return dialogPromise;
     }
