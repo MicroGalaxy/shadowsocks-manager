128c5e2648aeb02e65b4f77ec1b165c394ef53d6
MicroGalaxy <327288751@qq.com>
2025-06-25 19:12:39 +0800

    Fix running too slow

--- plugins/account_checker/index.js
+++ plugins/account_checker/index.js
@@ -553,7 +553,7 @@ cron.minute(async () => {
           const serverId = +accountLeft.split(':')[0];
           const accountId = +accountLeft.split(':')[1];
           const start = Date.now();
-          await checkAccount(serverId, accountId).catch(err => {});
+          checkAccount(serverId, accountId).catch(err => {});
           if(Date.now() - start < (1000 / speed)) {
             await sleep(1000 / speed - (Date.now() - start));
           }
@@ -562,7 +562,7 @@ cron.minute(async () => {
           const serverId = +accountRight.split(':')[0];
           const accountId = +accountRight.split(':')[1];
           const start = Date.now();
-          await checkAccount(serverId, accountId).catch(err => {});
+          checkAccount(serverId, accountId).catch(err => {});
           if(Date.now() - start < (1000 / speed)) {
             await sleep(1000 / speed - (Date.now() - start));
           }
@@ -570,7 +570,7 @@ cron.minute(async () => {
       }
       catch(err) {
         logger.error(err);
-        await sleep(5000);
+        await sleep(1000);
       }
     }
   } else {
